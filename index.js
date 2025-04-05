8284d330adadb3a11546adcada3e834c7c12a96e
Andrew Kungu <drewskungu@gmail.com>
2025-04-05 18:25:11 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let bod = document.querySelector("body")
+let main = bod.querySelector("main")
+bod.removeChild(main)
+
+let newHeader = document.createElement("h1")
+newHeader.id="victory"
+
+newHeader.textContent="Andrew is the champion"
